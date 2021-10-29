acb82cae4ecada1cb34b7aaabe4196533a965900
lizizilo <68889991+mynameisKt@users.noreply.github.com>
2021-10-29 14:28:32 +0800

    Add files via upload

--- js/Fallingstars.js
+++ js/Fallingstars.js
@@ -1,1 +1,26 @@
-// 用户回车搜索功能
+
+
+
+$(document).ready(function(){
+    // 点击切换搜素引擎
+    // bing.com
+    $("span.bing").click(function(){
+        $('.searchform').attr("action","https://cn.bing.com/search");
+        $('span.bing').addClass('baco');
+    });
+    //baidu.com
+    $("span.baidu").click(function(){
+        $('.searchform').attr("action","https://www.baidu.com/s");
+        $('span.baidu').addClass('baco');
+    });
+    // Google.com
+    $("span.google").click(function(){
+        $('.searchform').attr("action","https://www.google.com/search");
+        $('span.google').addClass('baco');
+    });
+
+    $("span.Wtclik").click(function(){
+        $('div.Wtdiv').addClass('Wtddivs');
+    });
+
+});
