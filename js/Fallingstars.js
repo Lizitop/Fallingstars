06993e998f2283b74879985cfea7ec688165d004
Zi Long <68889991+mynameisKt@users.noreply.github.com>
2021-11-12 19:56:50 +0800

    Add files via upload

--- js/Fallingstars.js
+++ js/Fallingstars.js
@@ -1,21 +1,29 @@
-
-
-
 $(document).ready(function(){
     // 点击切换搜素引擎
     // bing.com
     $("span.bing").click(function(){
         $('.searchform').attr("action","https://cn.bing.com/search");
-        $('span.bing').addClass('baco');
     });
     //baidu.com
     $("span.baidu").click(function(){
         $('.searchform').attr("action","https://www.baidu.com/s");
-        $('span.baidu').addClass('baco');
     });
     // Google.com
     $("span.google").click(function(){
         $('.searchform').attr("action","https://www.google.com/search");
-        $('span.google').addClass('baco');
     });
+
+    //js获取系统当前时间
+    var time = new Date();//获取系统当前时间
+    var hour= time.getHours();//得到小时数
+    var minute= time.getMinutes();//得到分钟数
+
+    // 当分钟数小于10时在minute前面加上0
+    // if (minute<10) {
+    //     minute='0'+minute;
+    //     return minute;
+    // }
+
+    document.getElementById("spanhours").innerHTML=hour;
+    document.getElementById("spanminute").innerHTML=minute;
 });
